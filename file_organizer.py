805d91f839e605a214a0cd690b814825209f9d93
WBeard4 <149584469+WBeard4@users.noreply.github.com>
2023-11-29 12:51:30 +0000

    Created Docs folder
    includes .txt, .pdf, .xlsx, .docx

--- file_organizer.py
+++ file_organizer.py
@@ -8,6 +8,7 @@ image = (".jpg", ".jpeg", ".png", ".gif", ".bmp")
 three_d = (".3ds", ".obj", ".stl", ".ctb", ".3mf", ".STL")
 zip = (".zip", ".rar", ".7z")
 executables = (".exe", ".msi")
+doc = (".txt", ".pdf", ".xlsx", ".docx")
 
 # Checking the file extension, to decide which folder the file will be moved to
 def is_video(file):
@@ -28,13 +29,18 @@ def is_zip(file):
 def is_exe(file):
     return os.path.splitext(file)[1] in executables
 
+def is_doc(file):
+    return os.path.splitext(file)[1] in doc
+
+
+
 def organize():
     # Assigning base directory here, so it can be used in os.path later
-    base_dir = "C:/Users/study/downloads"
+    base_dir = "C:/Users/will.beard/downloads"
     os.chdir(base_dir)
 
     # Checking if the folders exist, and if not creating them
-    folders = ['videos', 'audio', 'images', '3d', 'zips', 'executables', 'other']
+    folders = ['videos', 'audio', 'images', '3d', 'zips', 'executables', 'docs', 'other']
     for folder in folders:
         folder_path = os.path.join(base_dir, folder)
         if not os.path.exists(folder_path):
@@ -55,6 +61,8 @@ def organize():
                 shutil.move(file, os.path.join(base_dir, "zips", file))
             elif is_exe(file):
                 shutil.move(file, os.path.join(base_dir, "executables", file))
+            elif is_doc(file):
+                shutil.move(file, os.path.join(base_dir, "docs", file))
             else:
                 shutil.move(file, os.path.join(base_dir, "other", file))
             
